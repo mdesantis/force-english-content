04ae8ebef6f11abbd2c4c32bea2df88bb5c4d60c
Maurizio De Santis <desantis.maurizio@gmail.com>
2022-01-08 13:34:58 +0100

    Add 64x64 icon
    It adds 64x64 icon because it seems that Firefox Add-Ons homepage uses
    64x64 icon size.

--- scripts/build-icons.sh
+++ scripts/build-icons.sh
@@ -6,6 +6,6 @@ set -o nounset
 
 mkdir -p dist
 
-for SIZE in 16 32 48 96 128; do
+for SIZE in 16 32 48 64 96 128; do
 	inkscape --without-gui --export-width "${SIZE}" --export-height "${SIZE}" --export-png dist/icon"${SIZE}".png icon.svg
 done
