22e65ae1b945eae94f9873f36f6a01b49247ef61
Maurizio De Santis <maurizio.desantis@treatwell.com>
2022-12-09 17:54:19 +0100

    Fix error on assets copying when dist-* dirs already exist

--- scripts/web-ext-builds.sh
+++ scripts/web-ext-builds.sh
@@ -9,7 +9,8 @@ version=$(npm run version:echo --silent)
 for browser in chrome firefox; do
   dir=dist-"$browser"
 
-  cp -r dist "$dir"
+  mkdir -p "$dir"
+  cp dist/* "$dir"
   cp manifest-"$browser".json "$dir"/manifest.json
   npx web-ext build --source-dir "$dir" --artifacts-dir web-ext-artifacts/"$browser" --overwrite-dest --filename force_english_content-"$version"-"$browser".zip
 done
