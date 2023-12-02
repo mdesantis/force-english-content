0e0490cbae240370fc96ef464dd40d1f0bd56adc
Maurizio De Santis <desantis.maurizio@gmail.com>
2023-12-02 17:00:48 +0100

    Use Node.js latest LTS

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -4,10 +4,16 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-sudo apt-get install -y bash inkscape jq
+sudo apt-get install -y bash curl inkscape jq
 
 if ! command -v node &> /dev/null; then
-  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+  nodejs_latest_lts_major=$(
+    curl -fsSL https://nodejs.org/dist/index.json | \
+    jq --raw-output 'map(select(.lts)) | sort_by(.lts) | .[-1].version' | \
+    cut -d '.' -f 1 | \
+    tr -d 'v'
+  )
+  curl -fsSL "https://deb.nodesource.com/setup_$nodejs_latest_lts_major.x" | sudo -E bash -
   sudo apt-get install -y nodejs
 fi
 
