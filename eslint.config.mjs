e7c9427a08953d865a9e44d440707193b0120a1e
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-08-20 22:13:16 +0200

    Enable all ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  eslint.configs.recommended,
+  eslint.configs.all,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   {
@@ -15,5 +15,39 @@ export default tseslint.config(
       }
     }
   },
-  { ignores: ['dist', 'dist-*', 'eslint.config.mjs'] }
+  { ignores: ['dist', 'dist-*', 'eslint.config.mjs'] },
+  {
+    'rules': {
+      'capitalized-comments': ['warn', 'always', {
+        'ignoreConsecutiveComments': true
+      }],
+      'curly': ['error', 'multi-line', 'consistent'],
+      'func-style': ['error', 'declaration', {
+        'allowArrowFunctions': true
+      }],
+      'no-undefined': 'off',
+      'max-statements': ['error', {
+        'max': 15
+      }],
+      'one-var': ['error', 'never'],
+      'sort-imports': ['error', {
+        'allowSeparatedGroups': true
+      }]
+    }
+  },
+  {
+    'files': ['**/*.ts'],
+    'rules': {
+      'max-params': 'off',
+      'no-magic-numbers': 'off',
+      'no-shadow': 'off'
+    }
+  },
+  {
+    'files': ['**/*.spec.ts'],
+    'rules': {
+      'max-lines': 'off',
+      'max-lines-per-function': 'off'
+    }
+  }
 )
